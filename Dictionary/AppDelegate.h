e00eb3faf932c99303a8fa751eb61ded9190dc70
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-01-16 10:43:47 +0100

    Initial Commit

---
+++ Dictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dictionary
+//
+//  Created by Vicente Oliva de la Serna on 16/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dictionary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dictionary
+//
+//  Created by Vicente Oliva de la Serna on 16/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
