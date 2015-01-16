a0f026c733f2b73ed9f9a6703e8f27704276b113
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-01-16 15:01:08 +0100

    NavigationController Definition

---
+++ Dictionary/VOSDescriptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  VOSDescriptionViewController.h
+//  Dictionary
+//
+//  Created by Vicente Oliva de la Serna on 16/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VOSDescriptionViewController : UIViewController
+
+@end
---
+++ Dictionary/VOSDescriptionViewController.m
@@ -0,0 +1,37 @@
+//
+//  VOSDescriptionViewController.m
+//  Dictionary
+//
+//  Created by Vicente Oliva de la Serna on 16/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import "VOSDescriptionViewController.h"
+
+@interface VOSDescriptionViewController ()
+
+@end
+
+@implementation VOSDescriptionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
